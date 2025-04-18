c1c7e0e42a8ac727fb57ba460ba8c13649304b63
noryea <864460295@qq.com>
2025-04-18 18:38:07 +0800

    1.20.2~1.20.6

---
+++ neoforge/src/main/java/me/noryea/shieldsoundsbackport/ShieldSoundsBackportNeo.java
@@ -0,0 +1,23 @@
+package me.noryea.shieldsoundsbackport;
+
+import me.noryea.shieldsoundsbackport.handler.PlayerHurt;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.living.ShieldBlockEvent;
+
+@Mod("shieldsoundsbackport")
+public class ShieldSoundsBackportNeo {
+    public ShieldSoundsBackportNeo() {
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onLivingEntityHurt(ShieldBlockEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            PlayerHurt.onHurt(player, event.getDamageSource(), event.getOriginalBlockedDamage());
+        }
+        event.setCanceled(false);
+    }
+}
